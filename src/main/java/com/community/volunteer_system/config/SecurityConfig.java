ad3cdf8a06fea65d8f5e97f77763c404cf49a7fd
wasiullahkhan578 <wasiullahkhan578@gmail.com>
2026-04-30 10:47:45 +0530

    fixed cors

--- src/main/java/com/community/volunteer_system/config/SecurityConfig.java
+++ src/main/java/com/community/volunteer_system/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://volunteer-frontend-woad.vercel.app/login"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
         configuration.setAllowCredentials(true);
